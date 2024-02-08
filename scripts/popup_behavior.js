9bc627b91d700d2d75233b7d9a08426122027915
KodeUniverse <dakshin@gmail.com>
2024-02-07 21:03:16 -0500

    idk i'm high lol

---
+++ scripts/popup_behavior.js
@@ -0,0 +1,9 @@
+
+
+/**
+ * Listen for clicks on the buttons, and send appropriate msg to content script in the page
+ * 
+ */
+function listenForClicks() {
+    document.addEventListener("click", )
+}
